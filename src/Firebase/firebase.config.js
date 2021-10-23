7aa39cfcc1b58d02fddb94606247cb0cca5df283
arifulsajib <allaboutsajib@gmail.com>
2021-10-23 16:14:19 +0600

    done all

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4nQyk5zA_v7EpJ7ps7VxGXLbgJbXeZu4",
+  authDomain: "medisure-healthcare.firebaseapp.com",
+  projectId: "medisure-healthcare",
+  storageBucket: "medisure-healthcare.appspot.com",
+  messagingSenderId: "640889662426",
+  appId: "1:640889662426:web:95fa3141c158f2919b2ee3",
+};
+
+export default firebaseConfig;
